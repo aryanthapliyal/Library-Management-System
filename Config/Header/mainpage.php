f25a78193be6f981da8adeb7cca90f307dd9fb68
aryanthapliyal <88545389+aryanthapliyal@users.noreply.github.com>
2021-09-02 16:53:48 +0530

    Add files via upload

---
+++ Config/Header/mainpage.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>How to Create responsive Homepage</title>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700;900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="">    
+</head>
+<body>
+    <header>
+    <div class="wrapper">
+      
+<ul class="nav-area">
+<li><a href="homepage.php">Home</a></li>
+</ul>
+</div>
+<div class="bout">
+<h1>About us </h1>
+<p> this website is created witht the sense to share information on the quality and quantity of books available for registeration,pick up or request for new books that users would like to read </p>
+<p>you can further all even upload and preview images of books available for us</p>
+<p>For any further contact information :</p>
+<p>Contact Email:kokuyo@redif.com</p>
+<p>Contact Number:081725393</p>
+<p>Contact Number:817634593</p>
+<p>also try visiting us at Dombavili Doad 92 near Red Mary Hospital</p>
+</div>
+</header>
+
+
+</body>
+</html>
+
+<style>
+    
+    * {
+	margin: 0;
+	padding: 0;
+}
+body {
+	font-family: 'Poppins', sans-serif;
+}
+.wrapper {
+	width: 1170px;
+	margin: auto;
+}
+header {
+	background:  brown;
+	height: 100vh;
+	-webkit-background-size: cover;
+	background-size: cover;
+	background-position: center center;
+	position: relative;
+}
+.bout { color:white
+        
+}
+    </style>
+
+
