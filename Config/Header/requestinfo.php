2d0c33a35c2d90ef094a2a6f70590d98b0604cfd
aryanthapliyal <88545389+aryanthapliyal@users.noreply.github.com>
2021-09-15 21:47:04 +0530

    Update requestinfo.php

--- Config/Header/requestinfo.php
+++ Config/Header/requestinfo.php
@@ -1,24 +1,92 @@
-<!DOCTYPE HTML>
+<?php  include("DBConnection.php"); ?>
 <html>
 <body bgcolor="87ceeb">
-    <center><h2>Insert books Library Management System</h2></center>
+    <center><h2>request books Library Management System</h2></center>
     <br>
  
     <?php
-        include("DBConnection.php");
-        $libid=$_POST["libid"];
-        $fname=$_POST["fname"];
-        $isbn=$_POST["isbn"];
-        $title=$_POST["title"];
-        $author=$_POST["author"];
-        $edition=$_POST["edition"];
-        $publication=$_POST["publication"];
+       
+       if ($_SERVER["REQUEST_METHOD"]=="POST"){
+        if (empty($_POST["libid"])){ 
+            $libid = "fill out all the fields";
+            echo '<div style="font-size:3em">You have not filled library id! </div>';
+            exit($libid=$_POST["libid"]);
+           }
+         else { 
+            $libid=$_POST["libid"]; 
+           }    
+
+        if (empty($_POST["fname"])){ 
+            $fname = "fill out all the fields";
+            echo '<div style="font-size:3em">You have not filled name! </div>';
+            exit($fname=$_POST["fname"]);
+           }
+         else { 
+            $fname=$_POST["fname"]; 
+           }    
+
+        if (empty($_POST["isbn"])){ 
+           $isbn = "fill out all the fields";
+           echo '<div style="font-size:3em">You have not filled isbn! </div>';
+           exit($isbn=$_POST["isbn"]);
+          }
+        else { 
+           $isbn=$_POST["isbn"]; 
+          }    
+
+             if (empty($_POST["title"])){ 
+                $title = "fill out all the fields";
+                echo '<div style="font-size:3em">You have not filled title! </div>';
+                exit($title=$_POST["title"]);
+               }
+             else { 
+                $title=$_POST["title"]; 
+               }    
+
+               if (empty($_POST["author"])){ 
+                $author = "fill out all the fields";
+                echo '<div style="font-size:3em">You have not filled author! </div>';
+                exit($author=$_POST["author"]);
+               }
+             else { 
+                $author=$_POST["author"]; 
+               }    
+              
+               if (empty($_POST["edition"])){ 
+                $edition = "fill out all the fields";
+                echo '<div style="font-size:3em">You have not filled edition! </div>';
+                exit($edition=$_POST["edition"]);  
+             }
+             else { 
+                $edition=$_POST["edition"]; 
+               }    
+
+               if (empty($_POST["publication"])){ 
+                $publication = "fill out all the fields";
+                echo '<div style="font-size:3em">You have not filled publication! </div>';
+                exit($publication=$_POST["publication"]);  
+             }
+             else { 
+                $publication=$_POST["publication"]; 
+               }    
+    }
+     //   $libid=$_POST["libid"];
+       // $fname=$_POST["fname"];
+      //  $isbn=$_POST["isbn"];
+      //  $title=$_POST["title"];
+      //  $author=$_POST["author"];
+      //  $edition=$_POST["edition"];
+      //  $publication=$_POST["publication"];
  
         $query = "insert into books_request(libid,fname,isbn,title,author,edition,publication) values('$libid','$fname','$isbn','$title','$author','$edition','$publication')"; //Insert query to add book details into the book_info table
         $result = mysqli_query($db,$query);
     ?>
-    <h3> request is sent successfully </h3>
+    <h3> Use links below to navigate  </h3>
  
- 
+    <a href="SearchBooks.php"> To search for the Book information click here </a>
+    <div>
+    <a href="requestbooks.php"> To request for the Books click here </a>
+</div>
+    <a href="upload&fetchimg.php"> To upload or view picture of books click here </a>
 </body>
 </html>
