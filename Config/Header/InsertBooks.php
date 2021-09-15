7839edb78e0bfdf3f22382a749d4e8168c338fa6
aryanthapliyal <88545389+aryanthapliyal@users.noreply.github.com>
2021-09-15 21:36:46 +0530

    Update InsertBooks.php

--- Config/Header/InsertBooks.php
+++ Config/Header/InsertBooks.php
@@ -1,22 +1,70 @@
-<!DOCTYPE HTML>
+<?php  include("DBConnection.php"); 
+?>
 <html>
 <body bgcolor="87ceeb">
     <center><h2>Insert books Library Management System</h2></center>
     <br>
  
     <?php
-        include("DBConnection.php");
- 
-        $isbn=$_POST["isbn"];
-        $title=$_POST["title"];
-        $author=$_POST["author"];
-        $edition=$_POST["edition"];
-        $publication=$_POST["publication"];
+            $isbn = $title = $author = "";
+            $edition = $publication ="";
+     
+        
+        if ($_SERVER["REQUEST_METHOD"]=="POST"){
+             if (empty($_POST["isbn"])){ 
+                $isbn = "fill out all the fields";
+                echo '<div style="font-size:3em">You have not filled isbn! </div>';
+                exit($isbn=$_POST["isbn"]);
+               }
+             else { 
+                $isbn=$_POST["isbn"]; 
+               }    
+
+                  if (empty($_POST["title"])){ 
+                     $title = "fill out all the fields";
+                     echo '<div style="font-size:3em">You have not filled title! </div>';
+                     exit($title=$_POST["title"]);
+                    }
+                  else { 
+                     $title=$_POST["title"]; 
+                    }    
+
+                    if (empty($_POST["author"])){ 
+                     $author = "fill out all the fields";
+                     echo '<div style="font-size:3em">You have not filled author! </div>';
+                     exit($author=$_POST["author"]);
+                    }
+                  else { 
+                     $author=$_POST["author"]; 
+                    }    
+                   
+                    if (empty($_POST["edition"])){ 
+                     $edition = "fill out all the fields";
+                     echo '<div style="font-size:3em">You have not filled edition! </div>';
+                     exit($edition=$_POST["edition"]);  
+                  }
+                  else { 
+                     $edition=$_POST["edition"]; 
+                    }    
+
+                    if (empty($_POST["publication"])){ 
+                     $publication = "fill out all the fields";
+                     echo '<div style="font-size:3em">You have not filled publication! </div>';
+                     exit($publication=$_POST["publication"]);  
+                  }
+                  else { 
+                     $publication=$_POST["publication"]; 
+                    }    
+         }
+       // $title=$_POST["title"];
+      //  $author=$_POST["author"];
+       // $edition=$_POST["edition"];
+       // $publication=$_POST["publication"];
  
         $query = "insert into book_info(isbn,title,author,edition,publication) values('$isbn','$title','$author','$edition','$publication')"; //Insert query to add book details into the book_info table
         $result = mysqli_query($db,$query);
     ?>
-    <h3> Book information is inserted successfully </h3>
+    <h3> Use links below to navigate  </h3>
  
     <a href="SearchBooks.php"> To search for the Book information click here </a>
     <div>
@@ -25,3 +73,10 @@
     <a href="upload&fetchimg.php"> To upload or view picture of books click here </a>
 </body>
 </html>
+
+
+
+
+<?php
+
+?>
